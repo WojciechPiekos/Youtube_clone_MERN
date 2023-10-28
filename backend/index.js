fd2c22bc1f756ca484d6e012f72afda507563460
Wojciech Piekos <wojciech.piekos@gmail.com>
2023-10-28 15:53:41 +0200

    added backend models

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,17 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 
 import connectDB from "./config/dbCon.js"
+import usersRoutes from "./routes/users.js"
+import videosRoutes from "./routes/videos.js"
+import commentRoutes from "./routes/comments.js"
 
 const app = express()
 dotenv.config()
 connectDB()
 
+app.use("/api/users", usersRoutes)
+app.use("/api/videos", videosRoutes)
+app.use("/api/comments", commentRoutes)
 
 mongoose.connection.on("open", () => {
     console.log("Connected to MongoDB")
---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      unique: true,
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    subscribedUsers: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
